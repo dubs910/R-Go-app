fe3944b4d8ec86d426b524aae4355892d17e6757
optimus prime <78129976+dubs910@users.noreply.github.com>
2023-07-06 21:01:30 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Finalproject-app',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
