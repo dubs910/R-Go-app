6dde6b297122d56fca2adca4f58d007f1db35c18
BIBII23 <86736011+BIBII23@users.noreply.github.com>
2023-07-11 02:31:36 +0800

    Added more pictures for the products
    Edited the toolbar below, adding a details page when you clcik on products it will give you a preview for it(not yet finished)

--- code-files/src/app/pages/details/details.page.ts
+++ code-files/src/app/pages/details/details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Food } from 'src/app/models/food.model';
+import { FoodService } from 'src/app/services/food.service';
 
 @Component({
   selector: 'app-details',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.page.scss'],
 })
 export class DetailsPage implements OnInit {
+  id: number;
+  food!: Food;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private activatedRoute: ActivatedRoute, private foodService: FoodService) {
+    this.id = +this.activatedRoute.snapshot.paramMap.get('id')!
   }
 
+  ngOnInit() {
+    this.food = this.foodService.getFood(this.id)!;
+  }
 }
