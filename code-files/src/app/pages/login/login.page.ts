f1cacb7bb027a8844f5a85831456eb0a7bad9fbf
Nicopangilinan <nikoalfonsop@gmai.com>
2023-07-10 16:30:22 +0800

    Routing from login to home

--- code-files/src/app/pages/login/login.page.ts
+++ code-files/src/app/pages/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router} from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
-
+login(){
+ this.router.navigate(['/tabs']);
 }
+} 
