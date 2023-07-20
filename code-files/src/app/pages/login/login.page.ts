5223507d39fc0fd163aec226c00747662bdf2024
optimus prime <gabrielvalenton123@gmail.com>
2023-07-20 13:40:16 +0800

    Working login
    users.xml contains the credentials the user have to put

--- code-files/src/app/pages/login/login.page.ts
+++ code-files/src/app/pages/login/login.page.ts
@@ -1,17 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { Router} from '@angular/router';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
 
-  constructor(private router:Router) { }
+  constructor(private http: HttpClient, private router: Router) {}
 
-  ngOnInit() {
+  login() {
+    // Make a request to the PHP backend to check user credentials
+    this.http
+      .post<any>('http://localhost/login.php', {
+        email: this.email,
+        password: this.password,
+      })
+      .subscribe(
+        (response) => {
+          console.log('Response:', response); // Log the complete response
+          if (response.success) {
+            // Redirect to the dashboard or home page upon successful login
+            this.router.navigate(['/tabs']);
+          } else {
+            this.errorMessage = 'Invalid email or password.';
+          }
+        },
+        (error: HttpErrorResponse) => {
+          console.error('Error during login:', error);
+          this.errorMessage = 'An error occurred during login.';
+        }
+      );
   }
-login(){
- this.router.navigate(['/tabs']);
 }
-} 
--- code-files/src/app/pages/profile/profile.page.ts
+++ code-files/src/app/pages/profile/profile.page.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-
+  orders: any[] = []; // You can replace 'any[]' with the actual type of orders, such as 'Order[]'
+  
   constructor() { }
 
   ngOnInit() {
+    // Fetch the user's order history and assign it to the 'orders' property
+    this.fetchOrders();
   }
 
+  fetchOrders() {
+    // Implement the logic to fetch the user's order history from your database or service
+    // For now, I'll just use dummy data for demonstration purposes
+    this.orders = [
+      { orderNumber: '12345', totalAmount: 50.00, orderDate: new Date() },
+      { orderNumber: '54321', totalAmount: 30.00, orderDate: new Date() },
+      // Add more dummy orders as needed
+    ];
+  }
+
+  logout() {
+    // Implement the logout logic here, e.g., clearing session, redirecting to login page, etc.
+    console.log('User logged out');
+  }
 }
