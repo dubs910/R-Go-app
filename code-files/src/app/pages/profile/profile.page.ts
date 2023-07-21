e12e1488cda7a52560316b0d3e845a76794983b0
optimus prime <gabrielvalenton123@gmail.com>
2023-07-21 09:03:36 +0800

    Profile page not yet working huhu

--- code-files/src/app/pages/profile/profile.page.ts
+++ code-files/src/app/pages/profile/profile.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http'; // Import the HttpClient module
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
@@ -9,37 +9,75 @@ import { HttpClient } from '@angular/common/http'; // Import the HttpClient modu
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-  email: string = ''; // Declare the 'email' property
-  orders: any[] = []; // Declare the 'orders' property
+  email: string = ''; 
+  orders: any; 
+  xmlData: any;
 
   constructor(
     private router: Router,
     private authService: AuthService,
-    private http: HttpClient // Inject the HttpClient module
+    private http: HttpClient,
   ) {}
 
   ngOnInit() {
-    // Fetch the user data from the AuthService
     const user = this.authService.getUserData();
     this.email = user.email;
-    // Assign the email from the user data
-    // Fetch the user's order history and assign it to the 'orders' property
     this.fetchOrders();
+    this.fetchXMLData();
   }
 
   fetchOrders() {
-    // Make an HTTP GET request to the fetch_orders.php script that reads the XML data
-    this.http
-      .get<any[]>('http://localhost/fetch_orders.php') // Replace with the correct URL to your PHP script
+    // Create a new XMLHttpRequest object
+    const xhr = new XMLHttpRequest();
+
+    // Configure the GET request
+    xhr.open('GET', 'http://localhost/fetch_orders.php', true);
+
+    // Set the responseType to 'document' to parse XML data
+    xhr.responseType = 'document';
+
+    // Set up the onload callback to handle the response
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        // The request was successful
+        this.orders = xhr.response;
+      } else {
+        // The request failed
+        console.error('Error fetching order history.');
+      }
+    };
+
+    // Set up the onerror callback to handle errors
+    xhr.onerror = () => {
+      console.error('Error fetching order history.');
+    };
+
+    // Send the GET request
+    xhr.send();
+  }
+
+  fetchXMLData() {
+    this.http.get('http://localhost/fetch_orders.php', { responseType: 'text' })
       .subscribe(
         (response) => {
-          // Handle the response and assign the orders to the 'orders' property
-          this.orders = response;
+          // Parse the XML data
+          const parser = new DOMParser();
+          const xml = parser.parseFromString(response, 'text/xml');
+
+          // Convert the XML data to a JavaScript object (if needed)
+          const data = this.xmlToObject(xml);
+
+          // Store the XML data in the 'xmlData' property
+          this.xmlData = data;
         },
         (error) => {
-          console.error('Error fetching order history.', error);
+          console.error('Error fetching XML data.', error);
         }
       );
+  }
+
+  xmlToObject(xml: Document): any {
+    return xml;
   }
 
   logout() {
