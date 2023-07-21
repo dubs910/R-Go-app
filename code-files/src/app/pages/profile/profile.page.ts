b45301042dfd6a5233238c42f78549555e218712
optimus prime <gabrielvalenton123@gmail.com>
2023-07-21 11:14:19 +0800

    OKAY NA PLEASE WORK

--- code-files/src/app/pages/profile/profile.page.ts
+++ code-files/src/app/pages/profile/profile.page.ts
@@ -9,14 +9,8 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-<<<<<<< Updated upstream
-  email: string = ''; 
-  orders: any; 
-  xmlData: any;
-=======
   email: string = '';
   xmlData: any[] = []; // Initialize xmlData as an empty array
->>>>>>> Stashed changes
 
   constructor(
     private router: Router,
@@ -27,66 +21,6 @@ export class ProfilePage implements OnInit {
   ngOnInit() {
     const user = this.authService.getUserData();
     this.email = user.email;
-<<<<<<< Updated upstream
-    this.fetchOrders();
-    this.fetchXMLData();
-  }
-
-  fetchOrders() {
-    // Create a new XMLHttpRequest object
-    const xhr = new XMLHttpRequest();
-
-    // Configure the GET request
-    xhr.open('GET', 'http://localhost/fetch_orders.php', true);
-
-    // Set the responseType to 'document' to parse XML data
-    xhr.responseType = 'document';
-
-    // Set up the onload callback to handle the response
-    xhr.onload = () => {
-      if (xhr.status === 200) {
-        // The request was successful
-        this.orders = xhr.response;
-      } else {
-        // The request failed
-        console.error('Error fetching order history.');
-      }
-    };
-
-    // Set up the onerror callback to handle errors
-    xhr.onerror = () => {
-      console.error('Error fetching order history.');
-    };
-
-    // Send the GET request
-    xhr.send();
-  }
-
-  fetchXMLData() {
-    this.http.get('http://localhost/fetch_orders.php', { responseType: 'text' })
-      .subscribe(
-        (response) => {
-          // Parse the XML data
-          const parser = new DOMParser();
-          const xml = parser.parseFromString(response, 'text/xml');
-
-          // Convert the XML data to a JavaScript object (if needed)
-          const data = this.xmlToObject(xml);
-
-          // Store the XML data in the 'xmlData' property
-          this.xmlData = data;
-        },
-        (error) => {
-          console.error('Error fetching XML data.', error);
-        }
-      );
-  }
-
-  xmlToObject(xml: Document): any {
-    return xml;
-  }
-
-=======
     this.fetchXMLData(); 
   }
 
@@ -119,7 +53,6 @@ export class ProfilePage implements OnInit {
     );
   }
 
->>>>>>> Stashed changes
   logout() {
     // Call the logout method in the AuthService
     this.authService.logout();
